2974b198ca8c909e43e6bc90e9766dfa6b0c2d63
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-11 15:48:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.getElementById('incrementBtn').addEventListener('click', function() {
+      var counter = document.getElementById('counter');
+      var count = parseInt(counter.textContent, 10);
+      count++;
+      counter.textContent = count;
+      alert('Counter before increment: ' + count);
+    });
